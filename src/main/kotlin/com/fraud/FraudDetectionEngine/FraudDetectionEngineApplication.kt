322f81dba15e829efe96a38db44fe2b87c8fb6c9
mishalalquraini <alqurainimishal@gmail.com>
2025-05-04 20:03:22 +0300

    Update

--- src/main/kotlin/com/fraud/FraudDetectionEngine/FraudDetectionEngineApplication.kt
+++ src/main/kotlin/com/fraud/FraudDetectionEngine/FraudDetectionEngineApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class FraudDetectionEngineApplication
 
 fun main(args: Array<String>) {
+	val x = ""
 	runApplication<FraudDetectionEngineApplication>(*args)
 }
--- src/main/kotlin/com/fraud/FraudDetectionEngine/User/UserEntity.kt
+++ src/main/kotlin/com/fraud/FraudDetectionEngine/User/UserEntity.kt
@@ -1,2 +1,1 @@
 package com.fraud.FraudDetectionEngine.User
-
