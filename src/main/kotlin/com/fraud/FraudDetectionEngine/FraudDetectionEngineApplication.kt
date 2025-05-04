cbf5e7a367624def83fb773a984f4a0337d3af58
mishalalquraini <alqurainimishal@gmail.com>
2025-05-04 19:42:14 +0300

    Setup spring server

---
+++ src/main/kotlin/com/fraud/FraudDetectionEngine/FraudDetectionEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.fraud.FraudDetectionEngine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FraudDetectionEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<FraudDetectionEngineApplication>(*args)
+}
