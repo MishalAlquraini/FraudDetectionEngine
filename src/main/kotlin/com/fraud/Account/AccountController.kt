3845411e3ca57f60a3df88c20a9aeebc1c30978d
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-05-05 19:01:55 +0300

    Added create Account Feature

--- src/main/kotlin/com/fraud/Account/AccountController.kt
+++ src/main/kotlin/com/fraud/Account/AccountController.kt
@@ -1,23 +1,28 @@
 package com.fraud.Account
 
+import com.fraud.User.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import java.math.BigDecimal
+import java.security.Principal
 
 class AccountController (
     private val accountsService: AccountService,
+    private val userRepository: UserRepository
 
-    ){
+
+) {
 
     @PostMapping("/accounts/v1/accounts")
-    fun createAccount(@RequestBody request: CreateAccountRequest){
-            accountsService.createAccount(request.userId)
-            ResponseEntity.ok("Account created successfully")
+    fun createAccount(principal: Principal): ResponseEntity<String> {
+        val name = principal.name
+        val user = userRepository.findByName(name)
+            ?: return ResponseEntity.badRequest().body("User not found")
+
+        val userId = user.id ?: return ResponseEntity.badRequest().body("User ID is missing")
+        accountsService.createAccount(userId)
+
+        return ResponseEntity.ok("Account created successfully")
     }
 }
 
 
-data class CreateAccountRequest(
-    val userId: Long
-)
